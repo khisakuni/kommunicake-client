987733f8c88b01a049ecce29acbad8b83cea4451
Kohei Hisakuni <kohei@philosophie.is>
2018-01-07 22:52:36 -0800

    Make requst to login endpoint

--- src/scenes/Auth/Login/index.js
+++ src/scenes/Auth/Login/index.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react'
 import { Form, Text } from 'react-form'
+import _ from 'lodash'
+import { postLogin } from '../../../services/api/login'
+import { login } from '../../../services/auth'
+
+const errorStyles = {
+  color: 'red'
+}
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { error: null }
+
+    this.submit = this.submit.bind(this)
+    this.handleSuccess = this.handleSuccess.bind(this)
+    this.handleFailure = this.handleFailure.bind(this)
+  }
+
+  submit(values) {
+    postLogin(values).then(this.handleSuccess).catch(this.handleFailure)
+  }
+
+  handleSuccess(response) {
+    login({ user: response.user, token: response.token })
+  }
+
+  handleFailure(response) {
+    this.setState({ ...this.state, error: response.body.message })
+  }
+
+  displayError() {
+    return !_.isNil(this.state.error) && (
+      <span style={errorStyles}>There was an error: {this.state.error}</span>
+    )
+  }
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <Form>
+        <Form onSubmit={this.submit}>
           {
             (formApi) => (
-              <form>
+              <form onSubmit={formApi.submitForm}>
+                {this.displayError()}
+
                 <label htmlFor="email">Email</label>
                 <Text type="email" field="email" id="email"></Text>
 
                 <label htmlFor="password">Password</label>
                 <Text type="password" field="password" id="password"></Text>
+
+                <button type="submit" className="button">Login</button>
               </form>
             )
           }
