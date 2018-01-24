77394cab62974cbbcd6b0d0bfefea3910764f5d7
Kohei Hisakuni <kohei@philosophie.is>
2018-01-24 13:03:44 -0800

    Make App test run

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mockLocalStorage } from './test-helpers'
+
+beforeEach(() => {
+  mockLocalStorage()
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
